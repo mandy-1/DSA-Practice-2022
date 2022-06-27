b883304788ccafb97593b773b43637bffef8ffba
Mandeep Singh <74090377+mandy-1@users.noreply.github.com>
2022-06-27 09:51:59 +0530

    Time: 15 ms (71.01%), Space: 9 MB (37.40%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    int f(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
-        if(dp[i][j] != -1)return dp[i][j];
-        if(s1[i]==s2[j])return dp[i][j]= f(i-1,j-1,s1,s2,dp);
-        int del=1+f(i-1,j,s1,s2,dp);
-        int inc=1+f(i,j-1,s1,s2,dp);
-        int rep=1+f(i-1,j-1,s1,s2,dp);
-        return dp[i][j] = min(del,min(inc,rep));
-    }
+    // int f(int i,int j,string &s1,string &s2,vector<vector<int>> &dp){
+    //     if(i<0)return j+1;
+    //     if(j<0)return i+1;
+    //     if(dp[i][j] != -1)return dp[i][j];
+    //     if(s1[i]==s2[j])return dp[i][j]= f(i-1,j-1,s1,s2,dp);
+    //     int del=1+f(i-1,j,s1,s2,dp);
+    //     int inc=1+f(i,j-1,s1,s2,dp);
+    //     int rep=1+f(i-1,j-1,s1,s2,dp);
+    //     return dp[i][j] = min(del,min(inc,rep));
+    // }
     
     int minDistance(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s1,s2,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            dp[i][0]=i;
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else{
+                    int del=1+dp[i-1][j];
+                    int inc=1+dp[i][j-1];
+                    int rep=1+dp[i-1][j-1];
+                    dp[i][j] = min(del,min(inc,rep));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };

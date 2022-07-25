199b3ecb51da6a553b23666bd136d9fe5db44e5e
Mandeep Singh <74090377+mandy-1@users.noreply.github.com>
2022-07-25 18:30:31 +0530

    Time: 1090 ms (9.18%), Space: 10.4 MB (52.77%) - LeetHub

--- 312-burst-balloons/312-burst-balloons.cpp
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -5,8 +5,7 @@ public:
         int maxi=INT_MIN;
         if(dp[i][j] != -1)return dp[i][j];
         for(int k=i;k<=j;k++){
-            int ans = nums[i-1]*nums[k]*nums[j+1]+f(i,k-                                1,nums,dp)+f(k+1,j,nums,dp);
-            maxi = max(maxi,ans);
+            maxi = max(maxi,nums[i-1]*nums[k]*nums[j+1]+f(i,k-                                1,nums,dp)+f(k+1,j,nums,dp));
         }
         return dp[i][j] = maxi;
     }
@@ -14,7 +13,8 @@ public:
         
         nums.push_back(1);
         nums.insert(nums.begin(),1);
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return f(1,nums.size()-2,nums,dp);
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return f(1,n-2,nums,dp);
     }
 };

63d12b4ba534bc7d6c0d87f08baf9c4b2cc871a1
Mandeep Singh <74090377+mandy-1@users.noreply.github.com>
2022-10-25 15:59:43 +0530

    Added solution - LeetHub

---
+++ Maximum Meetings in One Room - GFG/maximum-meetings-in-one-room.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    static bool comp(vector<int>&a , vector<int>&b){
+        if(a[0]==b[0]) return a[2]<b[2];
+        return a[0]<b[0];
+    }
+
+    vector<int> maxMeetings(int n,vector<int> &s,vector<int> &f){
+        vector<vector<int>>v;
+        for(int i=0;i<n;i++){
+            v.push_back({f[i],s[i],i+1});
+        }
+        sort(v.begin(),v.end(),comp);
+        vector<int>ans;
+        int curr=0;
+        for(int i=0;i<n;i++){
+            if(v[i][1]>curr){
+                ans.push_back(v[i][2]);
+                curr=v[i][0];
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> S(n),F(n);
+        for(int i=0;i<n;i++){
+            cin>>S[i];
+        }
+        for(int i=0;i<n;i++){
+            cin>>F[i];
+        }
+        Solution ob;
+        vector<int> ans=ob.maxMeetings(n,S,F);
+        for(int i=0;i<ans.size();i++){
+            cout<<ans[i];
+            if(i!=ans.size()-1){
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
